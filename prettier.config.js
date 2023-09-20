926e0fbc12ce80b52031eb6409f20b63972dfc7e
Faizan Baig <faizan@showwcase.com>
2023-09-20 13:38:52 +0300

    formatted files and added repost icon

--- prettier.config.js
+++ prettier.config.js
@@ -2,4 +2,12 @@ module.exports = {
   singleQuote: true,
   semi: true,
   printWidth: 100,
-}
+  overrides: [
+    {
+      files: '*.svg',
+      options: {
+        parser: 'html',
+      },
+    },
+  ],
+};
